262dc4b7f1ce1e60ef4c2a185d4c2677d9952975
Martín <magadiflo@gmail.com>
2022-11-03 23:39:03 -0500

    Service interface

--- src/main/java/com/magadiflo/app/service/EmployeeService.java
+++ src/main/java/com/magadiflo/app/service/EmployeeService.java
@@ -1,14 +1,23 @@
 package com.magadiflo.app.service;
 
-public class EmployeeService {
+import com.magadiflo.app.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
 
     // Save an employee
+    void addEmployee(Employee employee);
 
     // Get employees
+    List<Employee> getAllEmployees();
 
     // Get one employee
+    Employee findById(Integer id);
 
     // Update employee
+    void updateEmployee(Employee employee);
 
     // Delete employee
+    Boolean deleteById(Integer id);
 }
