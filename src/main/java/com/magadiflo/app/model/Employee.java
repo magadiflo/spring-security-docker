2aadc61c3baca78b668357e756bece9c6488e036
Martín <magadiflo@gmail.com>
2022-11-03 23:32:15 -0500

    Domain Model

---
+++ src/main/java/com/magadiflo/app/model/Employee.java
@@ -0,0 +1,19 @@
+package com.magadiflo.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String username;
+
+}
---
+++ src/main/java/com/magadiflo/app/service/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.magadiflo.app.service;
+
+public class EmployeeService {
+
+    // Save an employee
+
+    // Get employees
+
+    // Get one employee
+
+    // Update employee
+
+    // Delete employee
+}
