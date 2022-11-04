99695a22d6167d368001ced7de2051eea6c55fa3
Martín <magadiflo@gmail.com>
2022-11-03 23:06:17 -0500

    Proyecto base - con dependencias en el pom.xml

---
+++ README.md
@@ -0,0 +1,2 @@
+# [Spring Boot API with Spring Security and Docker](https://www.youtube.com/watch?v=0iNmWIi5rG4)
+Curso tomado del canal de youtube: **Get Arrays**
---
+++ src/main/java/com/magadiflo/app/SpringSecurityDockerApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDockerApplication.class, args);
+	}
+
+}
