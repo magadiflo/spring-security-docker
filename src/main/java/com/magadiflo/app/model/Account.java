d26ba8752ce36820170a21152c131323c86465d3
Martín <magadiflo@gmail.com>
2022-11-08 23:28:09 -0500

    Account Domain Model

---
+++ src/main/java/com/magadiflo/app/model/Account.java
@@ -0,0 +1,35 @@
+package com.magadiflo.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "accounts")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private boolean enabled = true;
+    private boolean credentialsExpired = false;
+    private boolean expired = false;
+    private boolean locked = false;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "accounts_roles",
+            joinColumns = @JoinColumn(name = "account_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
+
+}
